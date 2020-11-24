30fddd5790163b3e788e4589839f5010805b9d65
João Pedro <jpedrodss@gmail.com>
2020-11-23 22:24:52 -0300

    Login pronto.
    Página de login feita.

--- Edux/App.js
+++ Edux/App.js
@@ -1,13 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack'
+import { NavigationContainer } from '@react-navigation/native';
 import { StyleSheet, Text, View } from 'react-native';
+
+import Login from './pages/Login'
+
+const Stack = createStackNavigator();
+
+const Autenticado = () => {
+  return (
+    <Drawer.Navigator initialRouteName="Home">
+      <Drawer.Screen name="Home" component={Home} />
+      <Drawer.Screen name="Logout" component={Logout} />
+    </Drawer.Navigator>
+  )
+}
+
+const Logout = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <Text>Deseja realmente sair da aplicação?</Text>
+      <TouchableOpacity onPress={() => {
+        AsyncStorage.removeItem('@jwt');
+        navigation.push('Login');
+      }} style={styles.button}>
+        <Text style={{color: 'white'}}>Sair</Text>
+      </TouchableOpacity>
+    </View>
+  )
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Autenticado" component={Autenticado} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -18,4 +49,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button: {
+    backgroundColor: '#3b3b3b',
+    width: '50%',
+    padding: 10,
+    borderRadius: 6,
+    marginTop: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+}
 });
---
+++ Edux/pages/Login/index.jsx
@@ -0,0 +1,127 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, KeyboardAvoidingView, Platform, View, TextInput, TouchableOpacity, Image, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import { url } from '../../utils/constants'
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const Login = ({ navigation }) => {
+    const [email, setEmail] = useState('')
+    const [senha, setSenha] = useState('')
+
+    const salvar = async (value) => {
+        try {
+            await AsyncStorage.setItem('@jwt', value)
+        } catch (e) {
+            // saving error
+        }
+    }
+
+    const Logar = () => {
+
+        const corpo = {
+            email: email,
+            senha: senha
+        }
+
+        fetch(`${url}login`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(corpo)
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                if (data.status != 404) {
+                    alert('Seja bem vindo');
+
+                    salvar(data.token);
+                    navigation.push('Autenticado');
+                } else {
+                    alert('Email ou senha inválidos! :( ');
+                }
+            })
+
+    }
+
+    return (
+        <KeyboardAvoidingView
+            behavior={Platform.OS == "ios" ? "padding" : "height"}
+            style={styles.container}
+        >
+            <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+                <View style={styles.container}>
+                    <Text style={{fontWeight: 1000, fontSize: 80, color: 'white'}}>Edux</Text>
+                    <Text style={styles.textoHeader}>LOGIN</Text>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="Email"
+                        onChangeText={text => setEmail(text)}
+                        defaultValue={email}
+                        keyboardType={"email-address"}
+                        autoCorrect={false}
+                    />
+                    <TextInput
+                        style={styles.input}
+                        placeholder="Senha"
+                        onChangeText={text => setSenha(text)}
+                        defaultValue={senha}
+                        secureTextEntry={true}
+                    />
+                    <TouchableOpacity
+                        style={styles.button}
+                        onPress={Logar}
+                    >
+                        <Text style={styles.textButton}>Entrar</Text>
+                    </TouchableOpacity>
+                </View>
+            </TouchableWithoutFeedback>
+        </KeyboardAvoidingView>
+    )
+}
+
+export default Login;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#8404D9',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    textoHeader: {
+        color: 'white',
+        margin: 10,
+        fontWeight: 700,
+        marginBottom: 15,
+        fontSize: 24
+    },
+    input: {
+        width: 250,
+        height: 40,
+        color: '#8404D9',
+        backgroundColor: 'white',
+        borderColor: 'gray',
+        borderWidth: 1,
+        marginTop: 20,
+        padding: 5,
+        paddingLeft: 10,
+        borderRadius: 10,
+        fontWeight: 1000
+    },
+    button: {
+        backgroundColor: 'white',
+        width: 250,
+        padding: 10,
+        borderRadius: 10,
+        marginTop: 20,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    textButton: {
+        fontWeight: 1000,
+        color: '#8404D9'
+    }
+
+});

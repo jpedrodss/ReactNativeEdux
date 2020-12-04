ee955acbff5da0fc133b64bc849e1c4d771f4a4c
João Pedro <jpedrodss@gmail.com>
2020-12-04 08:45:43 -0300

    Método listar para turmas

--- Edux/pages/Turmas/index.jsx
+++ Edux/pages/Turmas/index.jsx
@@ -1,47 +1,41 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, KeyboardAvoidingView, Platform, View, TextInput, TouchableOpacity, Image, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
 import { url } from '../../utils/constants'
 
 export default function Turmas() {
   const [turmas, setTurmas] = useState([]);
 
-  fetch(url + 'turmas')
+  fetch(url + 'turma')
     .then(response => response.json())
     .then(data => {
       setTurmas(data.data);
-      limparCampos();
+      console.log(data)
     })
     .catch(err => console.error(err));
 
-  const Item = ({ item }) => {
+  const renderItem = ({ item }) => {
     return (
-      <View style={styles.item}>
-        <Text style={styles.text}>{item.descricao}</Text>
-      </View>
+      <Item descricao={item.descricao} />
     )
   }
 
-  const renderItem = ({ descricao }) => {
+  const Item = ({ descricao }) => {
     return (
-      <Item descricao={item.descricao} />
+      <View style={styles.item}>
+        <Text style={styles.text}>{descricao}</Text>
+      </View>
     )
   }
 
   return (
     <View style={styles.container} >
       <Text style={styles.textHeader} >TURMAS</Text>
-      <div className="container">
-        <div className="card">
-          <Image
-            style={styles.logo}
-            source={{
-              uri:
-                'https://www.sp.senai.br/',
-            }}
-          />
-          <h2 className="tituloCard">Desenvolvimento de Sistemas</h2>
-        </div>
-      </div>
+      <FlatList
+        data={turmas}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+      />
     </View>
   )
 }
@@ -49,15 +43,18 @@ export default function Turmas() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: "column",
-    backgroundColor: '#8404D9',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    fontFamily: "Titillium Web",
   },
-  image: {
-    flex: 1,
-    resizeMode: "cover",
-    justifyContent: "center"
+  textHeader: {
+    color: "#8404D9",
+    textAlign: "center",
+    fontSize: 30,
+    marginTop: 30,
+    fontFamily: "Titillium Web",
+    fontWeight: "700"
   },
   text: {
     color: "#8404D9",
@@ -70,6 +67,12 @@ const styles = StyleSheet.create({
     width: '3.5em',
     height: '3.5em',
     borderRadius: '4em',
+  },
+  item: {
+    backgroundColor: '#2a2a2a',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16
   },
   margemE: {
     marginLeft: '0.3em',
